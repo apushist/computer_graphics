e23443027bcaf44154db9f2cc22d87c3cad6dfec
ugrozhalka <uliana.ivashkova@gmail.com>
2025-10-20 02:10:45 +0300

    Complete tasks 1a and 1b

--- lab5/Form2.Designer.cs
+++ lab5/Form2.Designer.cs
@@ -20,40 +20,231 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			button1 = new Button();
-			SuspendLayout();
-			// 
-			// button1
-			// 
-			button1.Location = new Point(642, 12);
-			button1.Name = "button1";
-			button1.Size = new Size(154, 23);
-			button1.TabIndex = 0;
-			button1.Text = "Вернуться в меню";
-			button1.UseVisualStyleBackColor = true;
-			button1.Click += button1_Click;
-			// 
-			// Form2
-			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
-			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
-			Controls.Add(button1);
-			Name = "Form2";
-			Text = "L-системы";
-			ResumeLayout(false);
-		}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonReturn = new Button();
+            buttonLoad = new Button();
+            buttonGenerate = new Button();
+            canvas = new PictureBox();
+            labelIter = new Label();
+            numericUpDownIter = new NumericUpDown();
+            checkBoxRandom = new CheckBox();
+            labelRandomAngle = new Label();
+            trackBarRandomAngle = new TrackBar();
+            labelRandomLen = new Label();
+            trackBarRandomLen = new TrackBar();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            buttonGenerateTree = new Button();
+            ((System.ComponentModel.ISupportInitialize)canvas).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownIter).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarRandomAngle).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarRandomLen).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // buttonReturn
+            // 
+            buttonReturn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonReturn.Location = new Point(3, 4);
+            buttonReturn.Margin = new Padding(3, 4, 3, 4);
+            buttonReturn.Name = "buttonReturn";
+            buttonReturn.Size = new Size(150, 30);
+            buttonReturn.TabIndex = 0;
+            buttonReturn.Text = "Вернуться в меню";
+            buttonReturn.UseVisualStyleBackColor = true;
+            buttonReturn.Click += ButtonReturn_Click;
+            // 
+            // buttonLoad
+            // 
+            buttonLoad.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonLoad.Location = new Point(3, 44);
+            buttonLoad.Margin = new Padding(3, 4, 3, 4);
+            buttonLoad.Name = "buttonLoad";
+            buttonLoad.Size = new Size(150, 28);
+            buttonLoad.TabIndex = 1;
+            buttonLoad.Text = "Загрузить файл...";
+            buttonLoad.UseVisualStyleBackColor = true;
+            buttonLoad.Click += ButtonLoad_Click;
+            // 
+            // buttonGenerate
+            // 
+            buttonGenerate.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonGenerate.Location = new Point(3, 80);
+            buttonGenerate.Margin = new Padding(3, 4, 3, 4);
+            buttonGenerate.Name = "buttonGenerate";
+            buttonGenerate.Size = new Size(150, 29);
+            buttonGenerate.TabIndex = 2;
+            buttonGenerate.Text = "Сгенерировать";
+            buttonGenerate.UseVisualStyleBackColor = true;
+            buttonGenerate.Click += ButtonGenerate_Click;
+            // 
+            // canvas
+            // 
+            canvas.Location = new Point(12, 12);
+            canvas.Name = "canvas";
+            canvas.Size = new Size(742, 576);
+            canvas.TabIndex = 3;
+            canvas.TabStop = false;
+            canvas.Paint += Canvas_Paint;
+            // 
+            // labelIter
+            // 
+            labelIter.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            labelIter.Location = new Point(3, 197);
+            labelIter.Name = "labelIter";
+            labelIter.Size = new Size(150, 48);
+            labelIter.TabIndex = 4;
+            labelIter.Text = "Количество итераций:";
+            labelIter.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // numericUpDownIter
+            // 
+            numericUpDownIter.Location = new Point(3, 248);
+            numericUpDownIter.Maximum = new decimal(new int[] { 15, 0, 0, 0 });
+            numericUpDownIter.Name = "numericUpDownIter";
+            numericUpDownIter.Size = new Size(64, 27);
+            numericUpDownIter.TabIndex = 5;
+            numericUpDownIter.TextAlign = HorizontalAlignment.Center;
+            numericUpDownIter.Value = new decimal(new int[] { 4, 0, 0, 0 });
+            // 
+            // checkBoxRandom
+            // 
+            checkBoxRandom.AutoSize = true;
+            checkBoxRandom.Location = new Point(3, 310);
+            checkBoxRandom.Name = "checkBoxRandom";
+            checkBoxRandom.Size = new Size(119, 24);
+            checkBoxRandom.TabIndex = 6;
+            checkBoxRandom.Text = "Случайность";
+            checkBoxRandom.UseVisualStyleBackColor = true;
+            // 
+            // labelRandomAngle
+            // 
+            labelRandomAngle.Location = new Point(3, 362);
+            labelRandomAngle.Name = "labelRandomAngle";
+            labelRandomAngle.Size = new Size(150, 25);
+            labelRandomAngle.TabIndex = 7;
+            labelRandomAngle.Text = "Разброс угла (°): 5";
+            labelRandomAngle.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // trackBarRandomAngle
+            // 
+            trackBarRandomAngle.Location = new Point(3, 398);
+            trackBarRandomAngle.Maximum = 60;
+            trackBarRandomAngle.Name = "trackBarRandomAngle";
+            trackBarRandomAngle.Size = new Size(150, 35);
+            trackBarRandomAngle.TabIndex = 8;
+            trackBarRandomAngle.Value = 5;
+            // 
+            // labelRandomLen
+            // 
+            labelRandomLen.Location = new Point(3, 490);
+            labelRandomLen.Name = "labelRandomLen";
+            labelRandomLen.Size = new Size(150, 43);
+            labelRandomLen.TabIndex = 9;
+            labelRandomLen.Text = "Разброс длины (%): 10";
+            labelRandomLen.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // trackBarRandomLen
+            // 
+            trackBarRandomLen.Location = new Point(3, 536);
+            trackBarRandomLen.Maximum = 50;
+            trackBarRandomLen.Name = "trackBarRandomLen";
+            trackBarRandomLen.Size = new Size(150, 37);
+            trackBarRandomLen.TabIndex = 10;
+            trackBarRandomLen.Value = 10;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(buttonGenerateTree, 0, 3);
+            tableLayoutPanel1.Controls.Add(buttonReturn, 0, 0);
+            tableLayoutPanel1.Controls.Add(buttonLoad, 0, 1);
+            tableLayoutPanel1.Controls.Add(buttonGenerate, 0, 2);
+            tableLayoutPanel1.Controls.Add(trackBarRandomLen, 0, 19);
+            tableLayoutPanel1.Controls.Add(labelRandomLen, 0, 18);
+            tableLayoutPanel1.Controls.Add(trackBarRandomAngle, 0, 15);
+            tableLayoutPanel1.Controls.Add(labelRandomAngle, 0, 14);
+            tableLayoutPanel1.Controls.Add(checkBoxRandom, 0, 12);
+            tableLayoutPanel1.Controls.Add(labelIter, 0, 6);
+            tableLayoutPanel1.Controls.Add(numericUpDownIter, 0, 7);
+            tableLayoutPanel1.Location = new Point(760, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 20;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 18.0672264F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 36F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 37F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 58F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 14F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 12F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 48F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 32F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 19F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 11F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 32F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 23F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 33F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 41F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 43F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 43F));
+            tableLayoutPanel1.Size = new Size(156, 576);
+            tableLayoutPanel1.TabIndex = 11;
+            // 
+            // buttonGenerateTree
+            // 
+            buttonGenerateTree.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonGenerateTree.Location = new Point(3, 117);
+            buttonGenerateTree.Margin = new Padding(3, 4, 3, 4);
+            buttonGenerateTree.Name = "buttonGenerateTree";
+            buttonGenerateTree.Size = new Size(150, 49);
+            buttonGenerateTree.TabIndex = 11;
+            buttonGenerateTree.Text = "Сгенерировать дерево";
+            buttonGenerateTree.UseVisualStyleBackColor = true;
+            buttonGenerateTree.Click += ButtonGenerateTree_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(914, 600);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(canvas);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form2";
+            Text = "L-системы";
+            ((System.ComponentModel.ISupportInitialize)canvas).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownIter).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarRandomAngle).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarRandomLen).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
 
-		#endregion
+        #endregion
 
-		private Button button1;
-	}
+        private Button buttonReturn;
+        private Button buttonLoad;
+        private Button buttonGenerate;
+        private PictureBox canvas;
+        private Label labelIter;
+        private NumericUpDown numericUpDownIter;
+        private CheckBox checkBoxRandom;
+        private Label labelRandomAngle;
+        private TrackBar trackBarRandomAngle;
+        private Label labelRandomLen;
+        private TrackBar trackBarRandomLen;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button buttonGenerateTree;
+    }
 }
--- lab5/Form2.cs
+++ lab5/Form2.cs
@@ -1,26 +1,370 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.ExceptionServices;
 using System.Windows.Forms;
 
 namespace lab5
 {
-	public partial class Form2 : Form
-	{
-		public Form2()
-		{
-			InitializeComponent();
-		}
+    public partial class Form2 : Form
+    {
+        private OpenFileDialog ofd;
 
-		private void button1_Click(object sender, EventArgs e)
-		{
-			Close();
-			
-		}
-	}
+        private string axiom = "F";
+        private double angleDeg = 25.0;
+        private double initialDirDeg = 90.0;
+        private Dictionary<char, string> rules = [];
+        private string currentString = "";
+        private List<(PointF a, PointF b)> segments = [];
+
+        private float baseStep = 10f;
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            trackBarRandomAngle.Scroll += (s, e) => labelRandomAngle.Text = $"Разброс угла (°): {trackBarRandomAngle.Value}";
+            trackBarRandomLen.Scroll += (s, e) => labelRandomLen.Text = $"Разброс длины (%): {trackBarRandomLen.Value}";
+
+            ofd = new OpenFileDialog()
+            {
+                Filter = "Text files|*.txt;*.ls;*.l;*.cfg|All files|*.*"
+            };
+
+            rules.Clear();
+            axiom = "F";
+            angleDeg = 22.5;
+            initialDirDeg = 90;
+            rules['F'] = "FF-[-F+F+F]+[+F-F-F]";
+        }
+
+        private void ButtonReturn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonLoad_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+            try
+            {
+                ParseLFile(File.ReadAllLines(ofd.FileName));
+                MessageBox.Show("Файл загружен. Нажмите кнопку \"Сгенерировать\"", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при разборе файла: " + ex.Message);
+            }
+        }
+
+        private void ButtonGenerate_Click(object sender, EventArgs e)
+        {
+            int iter = (int)numericUpDownIter.Value;
+            currentString = GenerateLSystem(axiom, rules, iter);
+            InterpretToSegments(currentString);
+            canvas.Invalidate();
+        }
+
+        private void ButtonGenerateTree_Click(object sender, EventArgs e)
+        {
+            int iter = (int)numericUpDownIter.Value;
+            currentString = GenerateLSystem(axiom, rules, iter);
+            InterpretToSegmentsTree(currentString);
+            canvas.Invalidate();
+        }
+
+        private void ParseLFile(string[] lines)
+        {
+            rules.Clear();
+            if (lines.Length == 0) throw new Exception("Пустой файл!");
+
+            int index = 0;
+            while (index < lines.Length && string.IsNullOrWhiteSpace(lines[index])) index++;
+            if (index >= lines.Length) throw new Exception("Нет данных!");
+
+            var fisrt = lines[index].Trim();
+            var parts = fisrt.Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length < 1) throw new Exception("Первая строка должна содержать аксиому");
+            axiom = parts[0];
+
+            if (parts.Length >= 2 && double.TryParse(parts[1], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double ang))
+                angleDeg = ang;
+            if (parts.Length >= 3 && double.TryParse(parts[2], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double dir))
+                initialDirDeg = dir;
+
+            for (int i = index + 1; i < lines.Length; i++)
+            {
+                var line = lines[i].Trim();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                string left;
+                string right;
+                if (line.Contains("->"))
+                {
+                    var sp = line.Split(["->"], StringSplitOptions.None);
+                    left = sp[0].Trim();
+                    right = sp[1].Trim();
+                }
+                else
+                {
+                    var sp = line.Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries);
+                    if (sp.Length >= 2)
+                    {
+                        left = sp[0].Trim();
+                        right = sp[1].Trim();
+                    }
+                    else if (sp.Length == 1 && sp[0].Length >= 2)
+                    {
+                        left = sp[0][0].ToString();
+                        right = sp[1].Substring(1);
+                    }
+                    else
+                    {
+                        throw new Exception($"Не распознана запись правила: '{line}'");
+                    }
+                }
+                if (left.Length != 1) throw new Exception($"Левая часть правила должна содержать один символ: {left}");
+                var key = left[0];
+                rules[key] = right;
+            }
+        }
+
+        private string GenerateLSystem(string axiom, Dictionary<char, string> rules, int iterations)
+        {
+            var cur = axiom;
+            for (int k = 0; k < iterations; k++)
+            {
+                var sb = new System.Text.StringBuilder();
+                foreach (char c in cur)
+                {
+                    if (rules.TryGetValue(c, out var rep))
+                        sb.Append(rep);
+                    else
+                        sb.Append(c);
+                }
+                cur = sb.ToString();
+                if (cur.Length > 20_000_000)
+                {
+                    MessageBox.Show($"Строка стала слишком длинной на итерации {k}. Остановка.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    break;
+                }
+            }
+            return cur;
+        }
+
+        private void InterpretToSegments(string commands)
+        {
+            segments.Clear();
+
+            var stack = new Stack<(float x, float y, double angle)>();
+
+            float x = 0f, y = 0f;
+            double angle = initialDirDeg * Math.PI / 180.0;
+
+            float step = baseStep;
+
+            var rnd = new Random();
+            bool useRandom = checkBoxRandom.Checked;
+            double maxAngleJitter = trackBarRandomAngle.Value;
+            double maxLenJitter = trackBarRandomLen.Value;
+
+            foreach (char c in commands)
+            {
+                if (c == 'F' || c == 'f')
+                {
+                    double angleNoise = 0.0;
+                    double lenMultiplier = 1.0;
+
+                    if (useRandom)
+                    {
+                        angleNoise = (rnd.NextDouble() * 2 - 1) * (maxAngleJitter * Math.PI / 180.0);
+                        lenMultiplier = 1.0 + (rnd.NextDouble() * 2 - 1) * (maxLenJitter / 100.0);
+                    }
+
+                    double localAngle = angle + angleNoise;
+
+                    float newX = x + (float)(Math.Cos(localAngle) * step * lenMultiplier);
+                    float newY = y + (float)(Math.Sin(localAngle) * step * lenMultiplier);
+
+                    if (c == 'F')
+                        segments.Add((new PointF(x, y), new PointF(newX, newY)));
+
+                    x = newX;
+                    y = newY;
+                }
+                else if (c == '+')
+                {
+                    angle += (angleDeg * Math.PI / 180.0);
+                }
+                else if (c == '-')
+                {
+                    angle -= (angleDeg * Math.PI / 180.0);
+                }
+                else if (c == '[')
+                {
+                    stack.Push((x, y, angle));
+                }
+                else if (c == ']')
+                {
+                    if (stack.Count > 0)
+                    {
+                        var st = stack.Pop();
+                        x = st.x;
+                        y = st.y;
+                        angle = st.angle;
+                    }
+                }
+            }
+        }
+
+        private void InterpretToSegmentsTree(string commands)
+        {
+            segments.Clear();
+
+            var stack = new Stack<(float x, float y, double angle, int depth)>();
+
+            float x = 0f, y = 0f;
+            double angle = initialDirDeg * Math.PI / 180.0;
+            float step = baseStep;
+
+            int depth = 0;
+            int maxDepth = 0;
+
+            foreach (char c in commands)
+            {
+                if (c == '[') depth++;
+                else if (c == ']') depth--;
+                maxDepth = Math.Max(maxDepth, depth);
+            }
+
+            var rnd = new Random();
+            bool useRandom = checkBoxRandom.Checked;
+            double maxAngleJitter = trackBarRandomAngle.Value;
+            double maxLenJitter = trackBarRandomLen.Value;
+
+            using (Graphics g = Graphics.FromImage(new Bitmap(1, 1))) { }
+
+            var lines = new List<(PointF a, PointF b, int depth)>();
+
+            depth = 0;
+            foreach (char c in commands)
+            {
+                if (c == 'F' || c == 'f')
+                {
+                    double angleNoise = 0.0;
+                    double lenMultiplier = 1.0;
+                    if (useRandom)
+                    {
+                        angleNoise = (rnd.NextDouble() * 2 - 1) * (maxAngleJitter * Math.PI / 180.0);
+                        lenMultiplier = 1.0 + (rnd.NextDouble() * 2 - 1) * (maxLenJitter / 100.0);
+                    }
+
+                    double localAngle = angle + angleNoise;
+
+                    float newX = x + (float)(Math.Cos(localAngle) * step * lenMultiplier);
+                    float newY = y + (float)(Math.Sin(localAngle) * step * lenMultiplier);
+
+                    if (c == 'F')
+                        lines.Add((new PointF(x, y), new PointF(newX, newY), depth));
+
+                    x = newX;
+                    y = newY;
+                }
+                else if (c == '+')
+                    angle += (angleDeg * Math.PI / 180.0);
+                else if (c == '-')
+                    angle -= (angleDeg * Math.PI / 180.0);
+                else if (c == '[')
+                {
+                    stack.Push((x, y, angle, depth));
+                    depth++;
+                }
+                else if (c == ']')
+                {
+                    if (stack.Count > 0)
+                    {
+                        var st = stack.Pop();
+                        x = st.x;
+                        y = st.y;
+                        angle = st.angle;
+                        depth = st.depth;
+                    }
+                }
+            }
+
+            segments.Clear();
+            segments.AddRange(lines.Select(l => (l.a, l.b)));
+
+            canvas.Paint += (s, e) =>
+            {
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                e.Graphics.Clear(Color.White);
+
+                if (lines.Count == 0) return;
+
+                var xs = lines.SelectMany(s => new[] { s.a.X, s.b.X });
+                var ys = lines.SelectMany(s => new[] { s.a.Y, s.b.Y });
+                float minX = xs.Min(), maxX = xs.Max(), minY = ys.Min(), maxY = ys.Max();
+                float w = maxX - minX, h = maxY - minY;
+                if (w == 0) w = 1; if (h == 0) h = 1;
+                float pad = 20;
+                float scale = Math.Min((canvas.Width - 2 * pad) / w, (canvas.Height - 2 * pad) / h);
+                float offsetX = pad - minX * scale + (canvas.Width - 2 * pad - w * scale) / 2;
+                float offsetY = pad - minY * scale + (canvas.Height - 2 * pad - h * scale) / 2;
+
+                foreach (var seg in lines)
+                {
+                    float t = maxDepth == 0 ? 0 : (float)seg.depth / maxDepth;
+                    int r = (int)(139 + (0 - 139) * t);
+                    int g = (int)(69 + (128 - 69) * t);
+                    int b = (int)(19 + (0 - 19) * t);
+                    float thickness = Math.Max(1f, 5f * (1f - t));
+
+                    using (var pen = new Pen(Color.FromArgb(r, g, b), thickness))
+                    {
+                        var a = new PointF(seg.a.X * scale + offsetX, seg.a.Y * scale + offsetY);
+                        var bpt = new PointF(seg.b.X * scale + offsetX, seg.b.Y * scale + offsetY);
+                        e.Graphics.DrawLine(pen, a, bpt);
+                    }
+                }
+            };
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            e.Graphics.Clear(Color.White);
+
+            if (segments.Count == 0) return;
+
+            var xs = segments.SelectMany(s => new[] { s.a.X, s.b.X });
+            var ys = segments.SelectMany(s => new[] { s.a.Y, s.b.Y });
+            float minX = xs.Min(), maxX = xs.Max(), minY = ys.Min(), maxY = ys.Max();
+
+            float w = maxX - minX;
+            float h = maxY - minY;
+            if (w == 0) w = 1;
+            if (h == 0) h = 1;
+
+            float pad = 20;
+            float scaleX = (canvas.ClientSize.Width - 2 * pad) / w;
+            float scaleY = (canvas.ClientSize.Height - 2 * pad) / h;
+            float scale = Math.Min(scaleX, scaleY);
+
+            float offsetX = pad - minX * scale + (canvas.ClientSize.Width - 2 * pad - w * scale) / 2;
+            float offsetY = pad - minY * scale + (canvas.ClientSize.Height - 2 * pad - h * scale) / 2;
+
+            using (var pen = new Pen(Color.Black, 1))
+            {
+                foreach (var seg in segments)
+                {
+                    var a = new PointF(seg.a.X * scale + offsetX, seg.a.Y * scale + offsetY);
+                    var b = new PointF(seg.b.X * scale + offsetX, seg.b.Y * scale + offsetY);
+                    e.Graphics.DrawLine(pen, a, b);
+                }
+            }
+        }
+    }
 }
